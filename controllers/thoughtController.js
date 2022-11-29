0a67748196e4879730de7553d1d545fb40d00fe1
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-11-28 22:23:55 -0800

    controller outlines added

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,19 @@
 const {Thought, User} = require('../models');
+
+const thoughtController = {
+//GET all thoughts
+    getThoughts(){},
+//GET single thought by ID
+    getSingleThought(){},
+//POST a new thought : push to associated id to users thoughts arr
+    createNewThought(){},
+//PUT update thought by ID
+    updateThought(){},
+//DELETE remove thought by ID
+    deleteThought(){},
+}
+
+//POST create reaction stored in single thought's reactions arr
+//DELETE remove reaction by reactionID
+
+module.exports = thoughtController;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,24 @@
 const {Thought, User} = require('../models');
+
+const userController = {
+//GET all users
+    getUsers(req, res){
+        User.find()
+        .then((users) => res.JSON(users))
+        .catch((err) => res.status(500).JSON(err));
+    },
+//GET single user by ID
+    getSingleUser(){},
+//POST a new user
+    createUser(){},
+//PUT update user by ID
+    updateUser(){},
+//DELETE remove user by ID : cascade to remove all associated thoughts
+    deleteUser(){},
+}
+
+//POST add friend to user friend list
+//DELETE remove friend from users friend list
+
+
+module.exports = userController;
