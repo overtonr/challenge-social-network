da36e93340877ee389d351a82e19697d886c14b7
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-11-28 20:47:24 -0800

    outline for reaction schema added to thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,15 +1,24 @@
 const { Schema, model } = require("mongoose");
 const userSchema = require('./User');
-const reactionSchema = require('./Reaction');
 
-const thoughtSchema = new Schema({
+
+//subdocument
+const reactionSchema = new Schema({
+    reactionId: {},//mongoose objectID data type, default set to new objectId
+    reactionBody:{}, // string, required, 280 max
+    username:{}, //string, required
+    createdAt: {} //date, set default to current, getter to format timestamp on query
+});
+
+//parent document
+const thoughtSchema = new Schema(
+    {
     thoughtText:{
         //string, required, between 1 &280 characters
         type: String,
         required: true,
         minLength: 1,
         maxLength: 280,
-
     },
     createdAt:{
         //date, current time default, getter method to format on query
@@ -22,16 +31,24 @@ const thoughtSchema = new Schema({
         type: String,
         required: true,
     },
-    
     //linked user that created thought
     reactions:[reactionSchema]//array of nested doc created with reactionSchema
-    
     },
     {
-        toJSON: {
-            virtuals: true,
-        },
+        toJSON: {virtuals: true},
         id: false,
     }
 );
+
+
 //virtual: rectionCount, length of reactions array
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function(){
+        return this.reactions.length;
+    });
+
+//Initialize model
+const Thought = model("Thought", thoughtSchema);
+//Export model
+model.exports = Thought;
