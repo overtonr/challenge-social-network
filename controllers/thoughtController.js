1cb43f8b839000786d1da95cf9c0a5b8b4f8a3cc
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-12-12 22:25:24 -0800

    thought controller routes finished

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,62 +1,113 @@
-const {Thought, User} = require('../models');
+const { Thought, User } = require('../models');
 
 const thoughtController = {
-//GET all thoughts
-    getThoughts(req,res){
+    //GET all thoughts
+    getThoughts(req, res) {
         Thought.find()
-        .then((thoughts) => res.json(thoughts))
-        .catch((err) => res.status(500),json(err));
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500), json(err));
     },
-//GET single thought by ID
-    getSingleThought(req,res){
+
+    //GET single thought by ID
+    getSingleThought(req, res) {
         Thought.findOne({ _id: req.params.thoughtId })
-        .select('-__v')
-        .then((thought) =>
-        !thought
-            ? res.status(400).json({message: 'Invalid thought ID'})
-            : res.json(thought)
-        )
-        .catch((err) => res.status.json(err));
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'Invalid thought ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
     },
-//POST a new thought : push to associated id to users thoughts arr
-    createThought(req, res){
+
+    //POST a new thought : push to associated id to users thoughts arr
+    createThought(req, res) {
         Thought.create(req.body)
-        .then((thought) => res.json(thought))
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).json(err);
-        });
+            .then((thought) => {
+                return User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $addToSet: { thoughts: thought._id } },
+                    { new: true }
+                );
+            })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({
+                        message: 'Thought created, but invalid user ID',
+                    })
+                    : res.json('New thought created')
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
     },
-//PUT update thought by ID
-    updateThought(req,res){
+
+    //PUT update thought by ID
+    updateThought(req, res) {
         Thought.findOneAndUpdate(
-            {_id: req.params.thoughtId},
-            {$set: req.body},
-            {runValidators: true, new: true}
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
         )
-        .then((thought) =>
-        !thought
-        ? res.status(404).json({message: 'Invalid thought ID'})
-        : res.json(thought)
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'Invalid thought ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    //DELETE remove thought by ID
+    deleteThought(req, res) {
+        Thought.findOneAndRemove({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'Invalid thought ID' })
+                    : User.findOneAndUpdate(
+                        { thoughts: req.params.thoughtId },
+                        { $pull: { thoughts: req.params.thoughtId } },
+                        { new: true }
+                    )
+            )
+            .then((user) =>
+                !user
+                    ? res
+                        .status(404)
+                        .json({ message: 'Thought created, but invalid user ID' })
+                    : res.json({ message: 'Thought created' })
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    //POST create reaction stored in single thought's reactions arr
+    addReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
         )
-        .catch((err) => res.status(500).json(err));
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'Invalid thought ID' })
+                    : res.json(video)
+            )
+            .catch((err) => res.status(500).json(err))
     },
-//DELETE remove thought by ID
-    deleteThought(req,res){
-        Thought.findOneAndUpdate({ _id: req.params.thoughtId })
-        .then((thought) =>
-        !thought
-        ? res.status(404).json({message: 'Invalid thought ID'})
-        : User.deleteMany({ _id: { $in: thought.users }})
+
+    //DELETE remove reaction by reactionID
+    deleteReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
         )
-    .then(() => res.json({ message: 'Thought and users deleted'}))
-    .catch((err) => res.status(500).json(err));
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'Invalid thought ID' })
+                    : res.json(video)
+            )
+            .catch((err) => res.status(500).json(err));
     },
-//POST create reaction stored in single thought's reactions arr
-    addReaction(req,res){
-    },
-//DELETE remove reaction by reactionID
-    deleteReaction(req,res){}
 };
 
 module.exports = thoughtController;
